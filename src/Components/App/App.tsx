de843fe7ffc98f257250a7caa04dc67e5f17cca1
Ivan Khoda <ivankhoda@gmail.com>
2022-07-12 19:44:41 +0300

    Push to heroku

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -11,6 +11,7 @@ import { Services } from "../Services/Services";
 import { Sidebar } from "../Sidebar/Sidebar";
 import { WorkingPanel } from "../WorkingPanel/WorkingPanel";
 import "./App.style.scss";
+require("dotenv").config();
 
 export const App = () => {
   const routes = [
